fc7d2a05833dfa7bafb92d18e73013a1c08b3f7f
boyce01 <boycezhao64@gmail.com>
2016-01-15 21:10:08 +1300

    powerball to select numbers for the biggest prize!
    Good luck!

---
+++ powerball/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace powerball
+{
+    partial class pbForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lottoTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pbTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lottoTxt
+            // 
+            this.lottoTxt.Location = new System.Drawing.Point(313, 137);
+            this.lottoTxt.Name = "lottoTxt";
+            this.lottoTxt.Size = new System.Drawing.Size(265, 25);
+            this.lottoTxt.TabIndex = 0;
+            this.lottoTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(125, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Lotto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(125, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Powerball";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pbTxt
+            // 
+            this.pbTxt.Location = new System.Drawing.Point(313, 255);
+            this.pbTxt.Name = "pbTxt";
+            this.pbTxt.Size = new System.Drawing.Size(265, 25);
+            this.pbTxt.TabIndex = 6;
+            this.pbTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(45, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(611, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "If you win numbers, do not forget to buy fish and chips for Boyce! ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Good luck";
+            // 
+            // pbForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 360);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbTxt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lottoTxt);
+            this.Name = "pbForm";
+            this.Text = "POWERBALL";
+            this.Load += new System.EventHandler(this.pbForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lottoTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox pbTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ powerball/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace powerball
+{
+    public partial class pbForm : Form
+    {
+        public pbForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pbForm_Load(object sender, EventArgs e)
+        {
+            lottoTxt.Text = "请随机选择Lotto";
+            pbTxt.Text = "请随机选择Powerball";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lottoTxt.Text = "";
+            //1~40随机选6，并从小到达排列，中间以"-"隔开
+            ArrayList list = new ArrayList(); //要添加using System.Collections命名空间
+            Random r = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                int lottoInt = r.Next(1, 41);
+                if (list.Contains(lottoInt)) //Contains()方法:判断ArrayList数组中是否存在某个元素
+                {
+                    i--; //若数组中存在该元素，则i自动减1，本次循环不算    
+                }
+                else //数组中不存在该随机数
+                {
+                    list.Add(lottoInt); //把该随机数添加到数组中
+                }
+            }
+            //把该数组从小到大排列
+            list.Sort();
+
+            //依次把数组中元素赋值给一个变量，并在每个数字用-连接
+            for (int i = 0; i < list.Count; i++)
+            {
+                lottoTxt.Text = lottoTxt.Text + list[i].ToString() + "-";
+            }
+            lottoTxt.Text = lottoTxt.Text.Substring(0, lottoTxt.Text.Length - 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            pbTxt.Text = r.Next(1, 11).ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
